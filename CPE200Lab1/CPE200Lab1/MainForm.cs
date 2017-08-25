cdddb8910c25909862dcbfb41a125805ec69d694
gear00000000 <gear_p413@hotmail.com>
2017-08-25 16:06:42 +0700

    เพิ่มบวกแบบต่อเนื่อง

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -12,7 +12,7 @@ namespace CPE200Lab1
 {
     public partial class MainForm : Form
     {
-        private bool hasDot;
+        private bool containDot;
         private bool isAllowBack;
         private bool isAfterOperater;
         private bool isAfterEqual;
@@ -23,7 +23,7 @@ namespace CPE200Lab1
         {
             lblDisplay.Text = "0";
             isAllowBack = true;
-            hasDot = false;
+            containDot = false;
             isAfterOperater = false;
             isAfterEqual = false;
         }
@@ -108,6 +108,24 @@ namespace CPE200Lab1
             {
                 return;
             }
+            if (firstOperand != null)
+            {
+                if (lblDisplay.Text is "Error")
+                {
+                    return;
+                }
+                string secondOperand = lblDisplay.Text;
+                string result = calculate (operate, firstOperand, secondOperand);
+                if (result is "E" || result.Length > 8)
+                {
+                    lblDisplay.Text = "Error";
+                }
+                else
+                {
+                    lblDisplay.Text = result;
+                }
+                isAfterEqual = true;
+            }
             if (isAfterOperater)
             {
                 return;
@@ -162,10 +180,10 @@ namespace CPE200Lab1
             {
                 return;
             }
-            if (!hasDot)
+            if (!containDot)
             {
                 lblDisplay.Text += ".";
-                hasDot = true;
+                containDot = true;
             }
         }
 
@@ -218,7 +236,7 @@ namespace CPE200Lab1
                 char rightMost = current[current.Length - 1];
                 if(rightMost is '.')
                 {
-                    hasDot = false;
+                    containDot = false;
                 }
                 lblDisplay.Text = current.Substring(0, current.Length - 1);
                 if(lblDisplay.Text is "" || lblDisplay.Text is "-")
